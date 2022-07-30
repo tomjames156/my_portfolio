d244e75fb5bac93832d3ce33556c5ea014e50278
Akinwande Tomisin <tomjames156@gmail.com>
2022-07-30 22:12:05 +0100

    added color coding, new project tiles styling and email functionality

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,41 @@
-let sendersName = document.querySelector("input#name");
-let sendersEmailAddress = document.querySelector("input#e-mail");
-let emailSubject = document.querySelector("input#subject");
-let emailBody = document.querySelector("input#message");
 let sendBtn = document.querySelector("a.send");
 let formElements = document.querySelectorAll("form > input");
+const myEmail = "tomjames156@gmail.com";
 
-console.log('fish');
+function tryFunc(){
+    let current_status = true;
 
-function tryFunc() {
-    for(let elmnt of formElements){
-        if(elmnt.value == ""){
-            sendBtn.style.display = "none";
-        }
-        else{
-            console.log("fish");
+    let emailSubject = document.querySelector("input#subject").value;
+    let emailBody = document.querySelector("input#message").value;
+
+    for(let element of formElements){
+        if(element.value == ""){
+            current_status = false;
+            sendBtn.removeAttribute("href");
+            break;
         }
     }
+
+    if(current_status){
+        console.log(emailSubject);
+        sendEmail(emailSubject, emailBody);
+    }
 }
+
+function sendEmail(subject, body){
+    sendBtn.setAttribute("href", `mailto:${myEmail}?subject=${separateCharacters(subject)}&body=${separateCharacters(body)}`);
+}
+
+function separateCharacters(sentence){
+    let words = sentence.split(" ");
+    let new_sentence = words[0];
+
+    for(let i = 1; i < words.length; i++){
+        new_sentence += `%20${words[i]}`;
+    }
+
+    return new_sentence;
+}
+
+// console.log(separateCharacters(sendersName));
+
