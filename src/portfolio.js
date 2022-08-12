9512f5e854091721df49fa76fbdd20e8de4c7328
Akinwande Tomisin <tomjames156@gmail.com>
2022-08-12 21:58:28 +0100

    update js to use a promise and change message body input to a textarea

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,26 +1,6 @@
 let sendBtn = document.querySelector("a.send");
-let formElements = document.querySelectorAll("form > input");
 const myEmail = "tomjames156@gmail.com";
-
-function sendMessage(){
-    let current_status = true;
-
-    let emailSubject = document.querySelector("input#subject").value;
-    let emailBody = document.querySelector("input#message").value;
-
-    for(let element of formElements){
-        if(element.value == ""){
-            current_status = false;
-            sendBtn.removeAttribute("href");
-            break;
-        }
-    }
-
-    if(current_status){
-        console.log(emailSubject);
-        addEmail(emailSubject, emailBody);
-    }
-}
+const validationSpace = document.getElementById("validation-result");
 
 function addEmail(subject, body){
     sendBtn.setAttribute("href", `mailto:${myEmail}?subject=${separateCharacters(subject)}&body=${separateCharacters(body)}`);
@@ -36,3 +16,32 @@ function separateCharacters(sentence){
 
     return new_sentence;
 }
+
+sendBtn.addEventListener('click', () =>{
+    let emailParams = [];
+    let sendersName = document.getElementById("name").value;
+    let emailSubject = document.getElementById("subject").value;
+    let emailBody = document.getElementById("message").value;
+
+    let setupEmail = new Promise(function(completeParams, incompleteParam){
+        if(sendersName && emailSubject && emailBody){
+            validationSpace.innerHTML = '';
+            emailParams = [emailSubject, emailBody];
+            completeParams(emailParams);
+        }else if(!(sendersName && emailSubject && emailBody)){
+            sendBtn.removeAttribute("href");
+            if(!sendersName){
+                incompleteParam("Please enter your name");
+            }else if(!emailSubject){
+                incompleteParam("Please enter the email's subject");
+            }else if(!emailBody){
+                incompleteParam("Please enter the email's body");
+            }
+        }
+    });
+
+    setupEmail.then(
+        function(value) {addEmail(value[0], value[1]);},
+        function(error) {validationSpace.innerHTML = error;}
+    );
+})
